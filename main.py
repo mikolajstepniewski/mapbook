6383eff44a60062cc1a270ca27b020fcefbf3260
mikolajstepniewski <168067675+mikolajstepniewski@users.noreply.github.com>
2024-05-23 12:15:48 +0200

    wip: add read function

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # name: str = input("Enter your name: ")
 # print(f'WITAJ {name}')
-data_off_users: list = [
+data_of_users: list = [
     {"name": "Julia", "surname": "Szklarzewska", "posts": 5, "location": "Hajnówka"},
     {"name": "Norbert", "surname": "Szeliga", "posts": 15, "location": "Rzeszów"},
     {"name": "Kacper", "surname": "Wójcik", "posts": 8, "location": "Legnica"},
     {"name": "Sebastian", "surname": "Wątroba", "posts": 12, "location": "Siedlce"},
 ]
-print(f'Witaj {data_off_users[0]["name"]}')
-def read(users:list)->None:
+print(f'Witaj {data_of_users[0]["name"]}')
+
+
+def read(users: list) -> None:
+
     """
-    This is a function to show users from a list
+    show users from a list
     :param users: a list of users
     :return: None
     """
@@ -17,4 +20,21 @@ def read(users:list)->None:
     for user in data_off_users[1:]:
         print(f'twój znajomy {user["name"]}, opublikował: {user["posts"]}')
 
-read(data_off_users)
+
+# read(data_off_users)
+
+def add_user(users: list) -> None:
+
+    """
+    add user to a list
+    :param users: user list
+    :return: None
+    """
+    name: str = input("Enter your name: ")
+    surname: str = input("Enter your surname: ")
+    posts: int = int(input("Enter your number of posts: "))
+    location: str = input("Enter your location: ")
+    new_user: dict = {"name": name, "surname": "surname", "posts": posts, "location": location}
+    users.append(new_user)
+add_user(data_of_users)
+read(data_of_users)
